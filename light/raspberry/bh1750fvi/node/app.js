7724088c0378681a3f40688b84d20d3ba8ea707f
JoonHo Son <joonho.work@gmail.com>
2015-08-04 15:26:12 +0900

    * GPIO_PIN -> WIRING_PIN 변환 추가.

--- light/raspberry/bh1750fvi/node/app.js
+++ light/raspberry/bh1750fvi/node/app.js
@@ -6,11 +6,10 @@ var wpi = require('wiring-pi');
 var isInit = false;
 var MAX_LUX = 65535;
 var REVISION = 5000;
+var LED_PIN = 18;
+var convertedPin;
 
-wpi.setup('wpi');
-wpi.pinMode(1, wpi.PWM_OUTPUT);
-
-measurementModes = {
+var MEASURE_MODE = {
   'Continuous_H_Resolution_Mode': 0x10,
   'Continuous_H_Resolution_Mode2': 0x11,
   'Continuous_L_Resolution_Mode': 0x13,
@@ -19,12 +18,67 @@ measurementModes = {
   'OneTime_L_Resolution_Mode': 0x23
 };
 
+var GPIO_PIN = [
+    2,
+    3,
+    4, 14,
+    15,
+    17, 18,
+    27,
+    22, 23,
+    24,
+    10,
+    9, 25,
+    11, 8,
+    7,
+    0, 1,
+    5,
+    6, 12,
+    13,
+    19, 16,
+    26, 20,
+    21
+];
+
+var WIRING_PIN = [
+    8,
+    9,
+    7, 15,
+    16,
+    0, 1,
+    2,
+    3, 4,
+    5,
+    12,
+    13, 6,
+    14, 10,
+    11,
+    30, 31,
+    21,
+    22, 26,
+    23,
+    24, 27,
+    25, 28,
+    29
+];
+
 function init() {
 	if (isInit) {
 		return true;
 	}
 
-	wpi.softPwmCreate(1, 100, 100);
+	convertedPin = convertPin(LED_PIN);
+
+	if (convertedPin === -1) {
+		console.log('pin error');
+
+		process.exit(1);
+	}
+
+	wpi.setup('wpi');
+	wpi.pinMode(convertedPin, wpi.PWM_OUTPUT);
+
+	wpi.softPwmCreate(convertedPin, 100, 100);
 
 	async.waterfall([
 		// power mode
@@ -52,13 +106,27 @@ function init() {
 	return isInit;
 }
 
+function convertPin(gpioPin) {
+    var wiringLength, i;
+
+    wiringLength = WIRING_PIN.length;
+
+    for (var i = 0; i < wiringLength; i++) {
+        if (GPIO_PIN[i] == gpioPin) {
+            return WIRING_PIN[i];
+        }
+    }
+
+    return -1;
+}
+
 (function getLux() {
 	if (init()) {
 		async.series([
 			function (callback) {
 				var hi = 0, lo = 0, li, lightValue;
 
-				wire.readBytes(measurementModes['Continuous_H_Resolution_Mode'], 2, function(err, bytes) {
+				wire.readBytes(MEASURE_MODE['Continuous_H_Resolution_Mode'], 2, function(err, bytes) {
 					if (err) {
 						callback(new Error('read fail.'), 'fail');
 					} else {
@@ -85,7 +153,7 @@ function init() {
 
 			console.log('light >>> ' + result);
 
-			wpi.softPwmWrite(1, parseInt(result));
+			wpi.softPwmWrite(convertedPin, parseInt(result));
 
 			// blaster.setPwm(18, result);
 		});
